41102e4c27162958c1d0d5398aba8cc1fdbf6cb7
Bill Moore <ksbillymoore@gmail.com>
2021-04-09 16:23:35 -0500

    Updates

---
+++ precache-manifest.0c8390210ff1638702a58dbac7dee2aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809956c1866467c90317871ee97388c5",
+    "url": "/billy-moore/treasure-game/index.html"
+  },
+  {
+    "revision": "dafe80b8e3a721075b9c",
+    "url": "/billy-moore/treasure-game/static/css/2.87d4210f.chunk.css"
+  },
+  {
+    "revision": "87657dc4823b334f219a",
+    "url": "/billy-moore/treasure-game/static/css/main.c41acbec.chunk.css"
+  },
+  {
+    "revision": "dafe80b8e3a721075b9c",
+    "url": "/billy-moore/treasure-game/static/js/2.5250161c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/billy-moore/treasure-game/static/js/2.5250161c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87657dc4823b334f219a",
+    "url": "/billy-moore/treasure-game/static/js/main.3fff7edd.chunk.js"
+  },
+  {
+    "revision": "9e5ab90011e7a3eac95a",
+    "url": "/billy-moore/treasure-game/static/js/runtime-main.4ddf8786.js"
+  },
+  {
+    "revision": "bd166282f53cfe8165a498b9c31da058",
+    "url": "/billy-moore/treasure-game/static/media/Landscape1.bd166282.png"
+  },
+  {
+    "revision": "3268a09b3c09eeba6bfec1cfe4d61344",
+    "url": "/billy-moore/treasure-game/static/media/Landscape6.3268a09b.png"
+  },
+  {
+    "revision": "ca2573ae521094897f271f319da8b3cb",
+    "url": "/billy-moore/treasure-game/static/media/LandscapeX.ca2573ae.png"
+  },
+  {
+    "revision": "42cdcd01f24720966ef8c15c5d4783fc",
+    "url": "/billy-moore/treasure-game/static/media/Lost.42cdcd01.png"
+  },
+  {
+    "revision": "876d337f9ee4e3e092954ec858292fef",
+    "url": "/billy-moore/treasure-game/static/media/Manchado.876d337f.png"
+  },
+  {
+    "revision": "6b61079513ce455f56fbe929dbad1893",
+    "url": "/billy-moore/treasure-game/static/media/OntheRun.6b610795.png"
+  },
+  {
+    "revision": "d53e7ad19e17490c162ee6f2fb9086f8",
+    "url": "/billy-moore/treasure-game/static/media/Tombstone.d53e7ad1.png"
+  }
+]);
